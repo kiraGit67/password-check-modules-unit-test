dfd486613db2bebb4e036087a10e1c1cba912322
kiraGit67 <kirsten@sais.de>
2023-06-02 12:57:06 +0200

    adds function isValid into password-utils.js

--- password-check-modules/password-utils.js
+++ password-check-modules/password-utils.js
@@ -1,5 +1,20 @@
 "use strict";
 
+import {
+  containsLowerCase,
+  containsUpperCase,
+  containsNumbers,
+} from "./string-utils.js";
+
 export function atLeast10charsLong(str) {
   return str.length >= 10;
+}
+
+export function isValid(pw) {
+  return (
+    containsNumbers(pw) &&
+    containsLowerCase(pw) &&
+    containsUpperCase(pw) &&
+    atLeast10charsLong(pw)
+  );
 }
