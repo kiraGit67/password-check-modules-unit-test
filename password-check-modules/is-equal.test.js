5376dd7595325efeb0eea27cc354894ad3fe216d
kiraGit67 <kirsten@sais.de>
2023-06-02 14:05:17 +0200

    adds two more test cases into password-check-modules/is-equal.test.js

--- password-check-modules/is-equal.test.js
+++ password-check-modules/is-equal.test.js
@@ -3,5 +3,7 @@ import { isEqual } from "./is-equal";
 describe("isEqual", function () {
   it("check if two passwords are equal", function () {
     expect(isEqual("Hallo", "Hallo")).toBe(true);
+    expect(isEqual("hello", "hallo")).toBe(false);
+    expect(isEqual("Hallo", "hallo")).toBe(false);
   });
 });
